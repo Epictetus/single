dc2e5923bf59e0a34c114715a6fdbf72cace3c90
willnet <netwillnet@gmail.com>
2012-06-30 16:56:49 +0900

    misc

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,13 @@
 class MainController < ApplicationController
+  caches_page :diag
+
   def index; end
 
   def diag
-    @twitter = Twitter.user_timeline(params[:name])
+    twitt = Twitter.user_timeline(params[:name], :count => 200)
+    timing = twitt.map(&:created_at)
+    weekends = timing.count {|t| t.saturday? || t.sunday? }
+    weekday_nights = timing.reject {|t| t.saturday? || t.sunday? }.select { |t| t.hour >= 21 || t.hour <= 3 }.tap { |t| p t}.count
+    @percent = (weekends + weekday_nights) / 2
   end
 end
